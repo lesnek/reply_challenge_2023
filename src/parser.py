cee10bfdaa6d3d8a1031c59870b4ba62d4497ada
Lesnek <milan.lesnek@kiwi.com>
2023-03-09 16:54:12 +0100

    statement a kousek parseru

--- src/parser.py
+++ src/parser.py
@@ -2,5 +2,7 @@ class InputParser:
     def __init__(self):
         ...
 
-    def parse(self, input_path) -> list[list[str], list[str]]:
-        ...
+    def parse(self, input_path: str) -> list[list[str], list[str]]:
+        with open(input_path, "w+") as filecek:
+            for line in filecek.read():
+                ...
